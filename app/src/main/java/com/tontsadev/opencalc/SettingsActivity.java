5aa448fff57ce858dd73cdee23ccbb8fb99d7d79
tontsa28 <xdman7917@gmail.com>
2021-06-16 16:59:31 +0300

    Remade toolbar properly

--- app/src/main/java/com/tontsadev/opencalc/SettingsActivity.java
+++ app/src/main/java/com/tontsadev/opencalc/SettingsActivity.java
@@ -1,25 +1,47 @@
 package com.tontsadev.opencalc;
 
+// Import AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity;
 
+// Import other classes
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
+
+// SettingsActivity class
 public class SettingsActivity extends AppCompatActivity {
 
+    // Initialize the activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Set activity_settings as the activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        ImageButton backBTN = findViewById(R.id.backBTN);
-        backBTN.setOnClickListener(view -> openMainActivity());
+        // Add toolbar to activity
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrowback_white);
+
     }
 
-    // Go back to calculator activity
-    public void openMainActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    // Initialize toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Add menu items to the toolbar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_settings, menu);
+        return true;
+
     }
 }
